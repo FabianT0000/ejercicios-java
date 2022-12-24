e6dbff4dd4d829df62b401e76601fc8d5bf31708
Didier Torres <fabiant1006@gmail.com>
2022-12-23 19:20:05 -0500

    segundo commit

--- 1/ejercicio1/src/App.java
+++ 1/ejercicio1/src/App.java
@@ -5,12 +5,12 @@ import java.util.*;
 import java.util.stream.*;
     
 import static java.util.stream.Collectors.toList;
-    
+    // mi primer commit
 public class App {
     
         public static void main(String[] args) throws IOException {
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-    #hola
+    
             int n = Integer.parseInt(bufferedReader.readLine().trim());
     
             List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
